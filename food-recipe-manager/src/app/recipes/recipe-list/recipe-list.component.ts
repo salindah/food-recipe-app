7c00369bf9a939f62850fd80eccd8d4974a169be
Salinda Hettiarachchi <shettiarachchi@cinglevue.com>
2020-04-08 23:58:46 +0200

    Added UI content for couple of components, added recipe model class

--- food-recipe-manager/src/app/recipes/recipe-list/recipe-list.component.ts
+++ food-recipe-manager/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes : Recipe[] = [
+    new Recipe('Pizza Recipe', 'Pizza', 'https://media-cdn.tripadvisor.com/media/photo-s/15/c5/a4/14/pepperoni-lovers.jpg'),
+    new Recipe('Sandwich Recipe', 'Sandwiches', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/egg-cress-club-sandwich_0.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
