6c17d210035d86c342450fb67fec0973a3f39ad7
Salinda Hettiarachchi <shettiarachchi@cinglevue.com>
2020-04-11 10:25:47 +0200

    Adding shopping list component

--- food-recipe-manager/src/app/shopping-list/shopping-list.component.ts
+++ food-recipe-manager/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingrediant } from '../shared/ingrediant.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingrediants: Ingrediant[] = [
+    new Ingrediant( "Apple", 10),
+    new Ingrediant( "Tomato", 5)
+  ];
 
   constructor() { }
 
